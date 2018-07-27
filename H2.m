42a55e78322922715ea66e71ab946992a14d518f
niccolotubini <tubini.niccolo@gmail.com>
2018-07-27 11:05:37 +0200

    Deleted since unnecessary

--- H2.m
+++
@@ -1,8 +0,0 @@
-%% Jordan decomposition: Hf = H1-H2
-
-function y = H2(psi)
-if(psi<0)
-    y = 0;
-else
-    y = H1(psi)-Hf(psi);
-end
--- dH2.m
+++
@@ -1,7 +0,0 @@
-%% Jordan decomposition: dH = dH1- dH2
-
-function y=dH2(psi)
-
-y = dH1(psi) - dH(psi);
-end
-
